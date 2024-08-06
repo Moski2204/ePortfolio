a2ac828d8f20875fa47e917fd2762337bb96bbef
Moski2204 <mahrusnaseem@gmail.com>
2024-08-06 15:04:19 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('darkMode').addEventListener('click', function() {
+  document.body.classList.toggle('dark-mode');
+  if (document.body.classList.contains('dark-mode')) {
+    document.getElementById('darkMode').innerHTML = 'Light Mode';
+  } else {
+    document.getElementById('darkMode').innerHTML = 'Dark Mode';
+  }
+});
