662d28b71d6876e58b9dbbec7a4d94f032f85c40
Moski2204 <mahrusnaseem@gmail.com>
2024-08-08 17:29:10 -0400

    Clarified Code

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-document.getElementById('darkMode').addEventListener('click', function() {
+document.getElementById('darkModeBtn').addEventListener('click', function() {
   document.body.classList.toggle('dark-mode');
   if (document.body.classList.contains('dark-mode')) {
-    document.getElementById('darkMode').innerHTML = 'Light Mode';
+    document.getElementById('darkModeBtn').innerHTML = 'Light Mode';
   }
   else {
-    document.getElementById('darkMode').innerHTML = 'Dark Mode';
+    document.getElementById('darkModeBtn').innerHTML = 'Dark Mode';
   }
 });
